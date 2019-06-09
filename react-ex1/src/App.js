b2fd8b16c9a88f2b7eb73ea6e89ec95af2cb2c85
Fernando <fherrera000@gmail.com>
2019-06-09 13:34:49 -0500

    commented out functional component in App.js and converted to class based component just for tutorial; Added button with onClick handler

--- react-ex1/src/App.js
+++ react-ex1/src/App.js
@@ -1,17 +1,46 @@
 import React from 'react';
+import { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi I'm a React App</h1>
-        <p> This is really working!</p>
-        <button>Switch Name</button>
-      <Person name="Fernando" age="35" />
-      <Person name="Max" age="28" >My Hobbies: Racing</Person>
-    </div>
-  );
+
+class App extends Component {
+    state = {
+        persons: [
+            { name: 'Max', age: 20 },
+            { name: 'Fernando', age: 35 }
+        ]
+    };
+
+    switchNameHandler = () => {
+        console.log('I got clicked!');
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <h1>Hi I'm a React App</h1>
+                <p> This is really working!</p>
+                <button onClick={this.switchNameHandler} >Switch Name</button>
+                <Person name="Fernando" age="35"/>
+                <Person name="Max" age="28">My Hobbies: Racing</Person>
+            </div>
+        );
+    }
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Hi I'm a React App</h1>
+//         <p> This is really working!</p>
+//         <button> onClick={this.switchNameHandler} Switch Name</button>
+//       <Person name="Fernando" age="35" />
+//       <Person name="Max" age="28" >My Hobbies: Racing</Person>
+//     </div>
+//   );
+// }
+
+
 
 export default App;
