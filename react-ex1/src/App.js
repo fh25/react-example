6d8439bb7cce54d32dd52644cc66a18df9043b8a
Fernando <fherrera000@gmail.com>
2019-06-09 13:21:24 -0500

    Added code to use props.children

--- react-ex1/src/App.js
+++ react-ex1/src/App.js
@@ -6,6 +6,8 @@ function App() {
   return (
     <div className="App">
       <h1>Hi I'm a React App</h1>
+        <p> This is really working!</p>
+        <button>Switch Name</button>
       <Person name="Fernando" age="35" />
       <Person name="Max" age="28" >My Hobbies: Racing</Person>
     </div>
