cae296f2ed56bd0761dedcdb8570134706369239
Fernando <fherrera000@gmail.com>
2019-06-09 13:07:00 -0500

    Passing props to component and using inside component

--- react-ex1/src/App.js
+++ react-ex1/src/App.js
@@ -6,11 +6,10 @@ function App() {
   return (
     <div className="App">
       <h1>Hi I'm a React App</h1>
-      <Person />
+      <Person name="Fernando" age="35" />
+      <Person name="Max" age="28" >My Hobbies: Racing</Person>
     </div>
   );
-  // below is what happens behind the scenes when using JSX:
-  //   return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
 }
 
 export default App;
--- react-ex1/src/Person/Person.js
+++ react-ex1/src/Person/Person.js
@@ -1,12 +1,7 @@
-//In it's simpliest form a component is just a function that returns some JSX
-//strongly recommended to use ES6 as best practice
-//could assign person a result of a function
-//function () {} would work fine but we want to use arrow function (ES6)
-//  so that we take advantage of new syntax and better handles this keyword
 import React from 'react';
 
-const person = () => {
-    return <p>I'm a person!</p>
+const person = (props) => {
+    return <p>I'm {props.name} and I'm {props.age} years old</p>
 };
 
 export default person;
