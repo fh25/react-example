7387e650b43f50ef074d453cbddfcfeab0a6a099
Fernando <fherrera000@gmail.com>
2019-06-09 20:28:18 -0500

    manipulating the state when a button is clicked

--- react-ex1/src/App.js
+++ react-ex1/src/App.js
@@ -9,11 +9,19 @@ class App extends Component {
         persons: [
             { name: 'Max', age: 20 },
             { name: 'Fernando', age: 35 }
-        ]
+        ],
+        otherState: 'some other value'
     };
 
     switchNameHandler = () => {
-        console.log('I got clicked!');
+        // console.log('I got clicked!');
+        // DON'T DO THIS this.state.persons[0].name = 'Roger'; can't edit state directly
+        this.setState({
+            persons: [
+                { name: 'Roger', age: 34 },
+                { name: 'Fernando', age: 35 }
+            ]
+        })
     };
 
     render() {
@@ -22,8 +30,8 @@ class App extends Component {
                 <h1>Hi I'm a React App</h1>
                 <p> This is really working!</p>
                 <button onClick={this.switchNameHandler} >Switch Name</button>
-                <Person name="Fernando" age="35"/>
-                <Person name="Max" age="28">My Hobbies: Racing</Person>
+                <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
             </div>
         );
     }
