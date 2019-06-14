96e80dc24f7bcff6e8a00f3fbc5192a97255506c
Fernando <fherrera000@gmail.com>
2019-06-13 23:01:38 -0500

    Recommended way to output content conditionally

--- react-ex1/src/App.js
+++ react-ex1/src/App.js
@@ -50,6 +50,19 @@ class App extends Component {
             cursor: 'pointer'
         };
 
+        let persons = null;
+
+        if (this.state.showPersons) {
+           persons = (
+               <div>
+                   <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+                   <Person name={this.state.persons[1].name} age={this.state.persons[1].age}
+                           click={this.switchNameHandler.bind(this, 'MAX!!')} changed={this.nameChangedHandler} />
+                   <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>My Hobbies: Racing</Person>
+               </div>
+           )
+        }
+
         return (
             <div className="App">
                 <h1>Hi I'm a React App</h1>
@@ -57,16 +70,7 @@ class App extends Component {
                 <button
                     style={style}
                     onClick={this.togglePersonsHandler} >Toggle Persons</button>
-                {
-                 this.state.showPersons ?
-                     <div>
-                        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-                        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}
-                                click={this.switchNameHandler.bind(this, 'MAX!!')} changed={this.nameChangedHandler} />
-                        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>My Hobbies: Racing</Person>
-                    </div>
-                    : null
-                }
+                {persons}
             </div>
         );
     }
